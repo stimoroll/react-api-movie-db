012b953512616c451abcea520826544e2d293040
Poplawski, Krzysztof [External] <Krzysztof.Poplawski@externals.adidas.com>
2019-08-27 14:40:22 +0200

    simple material ui formatting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, {useEffect,useState} from 'react';
+import {Grid,Typography} from '@material-ui/core/';
 import './App.css';
 
 import { instance, URL_WITH_KEY } from './services/movieService';
 
-const Movie = ({movie}) => {
+const MovieItem = ({movie}) => {
   const handleMovieClick = (event) => {
     console.log(event.currentTarget.getAttribute('data-id'));
   }
   return (
-    <p data-id={movie.imdbID} onClick={handleMovieClick}>
-      {movie.Title}
-      {movie.Poster}
-      {movie.Type}
-      {movie.Year}
-      {movie.imdbID}
-    </p>
+    <Grid item data-id={movie.imdbID} onClick={handleMovieClick}>
+      <Typography>{movie.Title}</Typography>
+      <Typography>{movie.Poster}</Typography>
+      <Typography>{movie.Type}</Typography>
+      <Typography>{movie.Year}</Typography>
+      <Typography>{movie.imdbID}</Typography>
+    </Grid>
   )
 }
 
+const MoviesList = ({movies, loading}) => (
+  <Grid container spacing={3}>
+    {(!loading && movies) && movies.map((movie, key)=><MovieItem key={key} movie={movie} />)}
+  </Grid>
+)
+
 const App = () => {
-  const [movies, setMovies] = useState([]);
   const [filter, setFilter] = useState('klan'); //TODO remove defalut value afeter coding
+  const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -53,7 +60,7 @@ const App = () => {
     <div className="App">
       <input type="search" onChange={handleFilterMovies} />
       {loading && <p>loading - please wait</p>}
-      {(!loading && movies) && movies.map((movie, key)=><Movie key={key} movie={movie} />)}
+      <MoviesList movies={movies} loading={loading} />
     </div>
   );
 }
