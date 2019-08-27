73c0a41d8860f24e006cef1a4d85e2812b649cd2
Poplawski, Krzysztof [External] <Krzysztof.Poplawski@externals.adidas.com>
2019-08-27 13:47:02 +0200

    fetch data with axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
+const config = {
+  OMDB_API_KEY: '2e01619a',
+  OMDB_API_URL: 'https://www.omdbapi.com/',
+}
+
+const tempTitle = 'klan';
 
 function App() {
+  useEffect(() => {
+    const instance = axios.create({
+      baseURL: `${config.OMDB_API_URL}`,
+      responseType: 'json',
+    });
+
+    const URL_WITH_KEY = `?apikey=${config.OMDB_API_KEY}`;
+
+    const fetchMovies = async (movieTitle) => {
+      try {
+        let posts = await instance.get(`${URL_WITH_KEY}&s=${movieTitle}`);
+        console.log(posts);
+      } catch(error)  {
+        console.log(error);
+      }
+    }
+
+    fetchMovies(tempTitle);
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
