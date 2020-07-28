6b94af776c8c3dd626a61bd59562920812b14f8d
Krzysztof Popławski <stimoroll@gmail.com>
2020-07-28 11:51:10 +0200

    update readme

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # REACT OMDB API test project
-* Axios
-* Material UI
-* Code splitting
+
+- React Hooks: useState, useEffects, useReducer
+- Axios
+- Material UI
+- Code splitting
 
 ## DEMO
+
 https://codesandbox.io/s/github/stimoroll/react-api-movie-db
 
 ## DEV
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
-### `npm start`
-### `npm run build`
-### `npm run eject`
 
-## TODO
+### `npm start`
+
+### `npm run build`
+
+### `npm run eject`
--- src/App.js
+++ src/App.js
@@ -1,51 +1,50 @@
-import React, {useEffect, useState, useContext, useReducer} from 'react';
-import {GET_MOVIE_BY_ID, OmdbIDContext} from './context';
-import {Grid,Typography} from '@material-ui/core/';
+import React, { useEffect, useState, useReducer } from "react";
+import { GET_MOVIE_BY_ID, OmdbIDContext } from "./context";
 
-import './App.css';
+import "./App.css";
 
-import { instance, URL_WITH_KEY } from './services/movieService';
-import SearchBar from './components/SearchBar';
-import MoviesList from './components/MovieList';
+import { instance, URL_WITH_KEY } from "./services/movieService";
+import SearchBar from "./components/SearchBar";
+import MoviesList from "./components/MovieList";
 
 //TODO: split into separate file
 //TODO: better formatting
-const MovieDetails = ({movie}) => (
+const MovieDetails = ({ movie }) => (
   <div>
-    <p>{movie.Actors}</p>
-    <p>{movie.Awards}</p>
-    <p>{movie.Country}</p>
-    <p>{movie.Director}</p>
-    <p>{movie.Title}</p>
-    <p>{movie.Year}</p>
+    <p>{movie?.Actors}</p>
+    <p>{movie?.Awards}</p>
+    <p>{movie?.Country}</p>
+    <p>{movie?.Director}</p>
+    <p>{movie?.Title}</p>
+    <p>{movie?.Year}</p>
   </div>
-)
+);
 
 //TODO moveout from here to file
 const initialState = {
-  omdbId: '',
-}
+  omdbId: ""
+};
 
 function reducer(state, action) {
   switch (action.type) {
     case GET_MOVIE_BY_ID:
       return {
-        omdbId: action.omdbId,
-      }
-      default:
-        return initialState
-      }
+        omdbId: action.omdbId
+      };
+    default:
+      return initialState;
+  }
 }
 
 const App = () => {
-  const [filter, setFilter] = useState('klan'); //TODO remove defalut value afeter coding
+  const [filter, setFilter] = useState("klan"); //TODO remove defalut value afeter coding
   const [movies, setMovies] = useState(false);
   const [movie, setMovie] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [omdbId, dispatch] = useReducer(reducer, initialState)
+  const [omdbId, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    const fetchMovies = async (movieTitle) => {
+    const fetchMovies = async movieTitle => {
       try {
         setLoading(true);
         let response = await instance.get(`${URL_WITH_KEY}&s=${movieTitle}`);
@@ -55,19 +54,19 @@ const App = () => {
         //if Search not exist => error
         //axios takes care about 30x, 40x => error
         console.log(response.data.Search);
-      } catch(error)  {
+      } catch (error) {
         console.log(error);
       } finally {
         setLoading(false);
       }
-    }
+    };
 
     fetchMovies(filter);
   }, [filter]);
 
   useEffect(() => {
     console.log(omdbId);
-    const fetchMovieDetails = async (omdbId) => {
+    const fetchMovieDetails = async omdbId => {
       try {
         setLoading(true);
         let response = await instance.get(`${URL_WITH_KEY}&i=${omdbId.omdbId}`);
@@ -75,22 +74,20 @@ const App = () => {
         //axios takes care about 30x, 40x => error
         setMovies(false);
         setMovie(response.data);
-        console.log(response.data);
-      } catch(error)  {
-        //TODO add display general info about 
-        console.log(error);
+      } catch (error) {
+        //TODO add display general info about
       } finally {
         setLoading(false);
       }
-    }
+    };
 
     fetchMovieDetails(omdbId);
   }, [omdbId]);
 
-  const handleFilterMovies = (event) => {
+  const handleFilterMovies = event => {
     console.log(event.currentTarget.value);
     setFilter(event.currentTarget.value);
-  }
+  };
 
   //TODO - better condition for dispaly MovieDetail or MovieList
   //TODO - better loader is required
@@ -101,11 +98,10 @@ const App = () => {
         <SearchBar handleFilterMovies={handleFilterMovies} />
         {loading && <p>loading - please wait</p>}
         {movies && <MoviesList movies={movies} loading={loading} />}
-        {movie && <MovieDetails movie={movie}/>}
-
+        {movie && <MovieDetails movie={movie} />}
       </div>
     </OmdbIDContext.Provider>
   );
-}
+};
 
 export default App;
